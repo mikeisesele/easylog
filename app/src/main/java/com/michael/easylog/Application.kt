5c7345e3e1ee2f9955b3e2b86f6592ccf5aa0a1f
michaelisesele <michaelisesele@gmail.com>
2024-06-09 16:12:49 +0100

    set up logging in application class

---
+++ app/src/main/java/com/michael/easylog/Application.kt
@@ -0,0 +1,26 @@
+package com.michael.easylog
+
+import android.app.Application
+import com.bugfender.sdk.Bugfender
+import timber.log.Timber
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        
+        // for Timber integration
+//      Timber.plant(Timber.DebugTree())
+
+        // for Bugfender integration
+//      Bugfender.init(this, BuildConfig.BUGFENDER_API_KEY, BuildConfig.DEBUG, true)
+//      Bugfender.enableUIEventLogging(this)
+//      Bugfender.enableLogcatLogging()
+
+        EasyLog.setUp {
+            debugMode(BuildConfig.DEBUG)
+            defaultLogger(DefaultLogger.DEFAULT_ANDROID)
+            filterTag ("Rigel")
+            build()
+        }
+    }
+}
